26c035584f325f4ef066d4dc9b791629d5c283bf
Miro <mairoenwa@gmail.com>
2025-06-09 23:51:18 -0600

    Project Completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,9 +6,11 @@ import dotenv, { parse } from 'dotenv';
 import connectDB from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import { app, server, io } from './lib/socket.js';
+import path from 'path';
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -19,6 +21,13 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  });
+}
 
 
 server.listen(PORT, () => {
